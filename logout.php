ed31f13ec044d093ec159348b77ced618777f8c6
Brian W <Briantechdom@gmail.com>
2018-05-02 16:59:06 -0700

    Completed logout.php content

--- logout.php
+++ logout.php
@@ -1,5 +1,12 @@
 <?php
 
+// did the user's browser send a cookie for the session?
+if(isset($_COOKIE[session_name()])) {
+
+  // empty cookie
+  setcookie(session_name(), '', time() - 86400, '/' );
+}
+
 // clear all session variables
 session_unset();
 
@@ -7,6 +14,9 @@ session_unset();
 session_destroy();
 
 echo "You've been logged out! See you next time. <br>";
+
+// log back in
+echo "<p><a href='login.php'> Log back in</a></p>";
 
 
 
